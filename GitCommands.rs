5e011a183a500155ef9caff70bc6210f80cc9092
Ujjawal Kumar <ujjawalsingh800007@gmail.com>
2024-12-22 00:11:21 +0530

    Rust Learning and Guessing Game

---
+++ GitCommands.rs
@@ -0,0 +1,37 @@
+// __________________________  To Push Codes  ____________________________________________________
+// _____________________________________________________________________________________________|_|
+//  -->>  git init                                                                              | |
+//  -->>  git add .                                                                             | |
+//  -->>  git status                                                                            | |
+//  -->>  git commit -m "Commits"                                                               | |
+//  -->>  git branch -M main                                                                    | |
+//  -->>  git remote add origin https://github.com/ujjawalsingh25/Rust.git                      | |
+//  -->>  git push -u origin main                                                               | |
+// ---------------------------------------------------------------------------------------------| |
+//    git reset  -->> To revoke back to Unstaging area                                          | |
+// ---------------------------------------------------------------------------------------------| |
+//   To revoke last Commit                                                                      | |
+//      -->>  git reset --soft HEAD~1                                                           | |
+//      -->>  git push origin main --force                                                      | |
+// _____________________________________________________________________________________________|_|
+// _____________________________________________________________________________________________|_|
+
+
+// __________________________  From GitHub  ________________________________________
+//                                                                                  |
+// …or create a new repository on the command line                                  |
+//                                                                                  | 
+// echo "# Rust" >> README.md                                                       |
+// git init                                                                         |
+// git add README.md                                                                |
+// git commit -m "first commit"                                                     |
+// git branch -M main                                                               |
+// git remote add origin https://github.com/ujjawalsingh25/Rust.git                 |
+// git push -u origin main                                                          |
+//                                                                                  |
+//                                                                                  |
+// …or push an existing repository from the command line                            |
+// git remote add origin https://github.com/ujjawalsingh25/Rust.git                 |
+// git branch -M main                                                               |
+// git push -u origin main                                                          |
+// _________________________________________________________________________________|
